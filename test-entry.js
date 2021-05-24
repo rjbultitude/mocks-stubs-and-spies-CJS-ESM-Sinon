4ca4c26ab1a227849c1917a7b249227839764b86
Richard Bultitude <richard.bultitude@gmail.com>
2021-05-24 14:27:02 +0100

    remove unnecessary file

--- test-entry.js
+++
@@ -1,8 +0,0 @@
-import { Spy } from './spy.js';
-import * as module_A from './module_A.js';
-
-console.log(module_A.someFn);
-
-const someFnspy = Spy(module_A, 'someFn');
-module_A.depInjtestFn();
-console.log('someFnspy.count', someFnspy.count);
